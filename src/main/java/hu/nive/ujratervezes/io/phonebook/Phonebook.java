12e10dae49df15f6bb95c7ab4fa4ba2064d1695f
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-04-29 11:11:53 +0200

    only nullPhoneBook is red

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,28 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        try (Writer writer = new FileWriter(output)) {
+
+            for (Map.Entry<String, String> contact : contacts.entrySet()) {
+                writer.append(contact.getKey())
+                        .append(": ").append(contact.getValue()).append("\n");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Cannot write file!");
+        } catch (NullPointerException ne) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
